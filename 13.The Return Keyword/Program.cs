128804e121cc0467d259b122a8ea79d2db07fe77
Nokwanda Khubisa <Nokwanda.Khubisa@standardbank.co.za>
2024-01-11 08:45:58 +0200

    Add project files.

---
+++ 13.The Return Keyword/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.The_Return_Keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling methods with a return types
+            Console.WriteLine(cube(5));
+            //this wont give anything to console
+            cube(5);
+
+            //freeze console
+            Console.ReadLine();
+        }
+        static int cube(int number)
+        {
+            int result = number * number * number;
+            return result;
+        }
+    }
+}
